10359b8d47bc7e4c41ee49aca06338f56ade402c
Rory Hewitt Scobie <scobier@hartford.cs.arizona.edu>
2017-09-11 19:57:53 -0700

    asdf

--- Hello.java
+++ Hello.java
@@ -12,20 +12,20 @@ public class Hello {
 
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-	System.out.println("Stabilize your rear deflectors... Watch for enemy fighters")
-	system.out.println("Punch it, Chewie!")
+	System.out.println("Stabilize your rear deflectors... Watch for enemy fighters");
+	system.out.println("Punch it, Chewie!");
 
     }
 
     public static void goldLeader() {
-system.out.println("wilhelm screen")
+system.out.println("wilhelm screen");
         // Gold Leader should add a print statement that says "Loosen up!"
 
 
     }
 
     public static void redLeader() {
-system.out.println("Lock S-foils in attack position.")
+system.out.println("Lock S-foils in attack position.");
         // Red Leader should add a print statement that says "I copy, gold leader!"
 
 
