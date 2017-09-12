9390af1669c601c5cf66b138bd380264aa67d858
jrwarshawer <jrwarshawer@email.arizona.edu>
2017-09-11 19:48:57 -0700

    Second go at it

--- Hello.java
+++ Hello.java
@@ -12,7 +12,7 @@ public class Hello {
 
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-
+system.out.println("Punch it, Chewie!")
     }
 
     public static void goldLeader() {
